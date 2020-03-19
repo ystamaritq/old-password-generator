57ece9938b7f88137bee226a0a7c0f9f8abb3310
Yadira Tamarit <ystamaritq@gmail.com>
2020-03-18 23:08:27 -0500

    added changes to the js file

--- Develop/script.js
+++ Develop/script.js
@@ -3,14 +3,15 @@
 function getPassLenght() {
   var pass_length = prompt("How many caracters do you like to have in the new password? Enter a valid number between 8 and 128");
   if (pass_length < 8 || pass_length > 128) {
-    confirm("Error, enter a valid number (your password need at least 8 caracters)");
+    alert("Error, enter a valid number (your password need at least 8 caracters)");
+    return -1;
   } else {
     return pass_length;
   }
 }
 
 //function get the character types to include in the password
-function getCharactersTypes() {
+function getCharSets() {
 
   //creating the character types
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
@@ -41,29 +42,36 @@ function getCharactersTypes() {
 
   //validate if the user select at least one group of character
   if (charSets.length < 1) {
-    confirm("Error you have to select at least one type of character");
-  } else {
-    return charSets;
+    alert("Error you have to select at least one type of character");
   }
+  return charSets;
 }
 
 //function generate password
 function generatePassword() {
-  
+
+  //variables
+  var lenght = getPassLenght();
+
+  if (lenght < 1)
+    return "";
+
+  var charSets = getCharSets();
+
+  if (charSets.length < 1)
+    return "";
+
+  var newPassword = '';
+
+  //for loop to generate the new password
+  for (var i = 0; i < length; i++) {
+    var charSetIndex = i % charSets.lenght;
+    var charSet = charSets[charSetIndex];
+    var randomIndex = 0; //generate random index
+    newPassword += charSet.charAt[randomIndex]; ///characterTypesSelected.charAt((Math.floor(Math.random)) * characterTypesSelected.length);
+  }
+  return newPassword;
 }
-
-
-// var caracters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-// var newpass = '';
-
-// var times = caracters.length;
-// if ((typeof pass_length === 'integer') && pass_length > 0) {
-//   for (var i = 1; i <= pass_length; i++) {
-//     newpass += caracters.charAt((Math.floor(Math.random)) * times);
-//   }
-// }
-// return newpass;
-
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
