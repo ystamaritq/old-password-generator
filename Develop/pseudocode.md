43882f5e12c8b60f8e78a8019a711267854c2a83
Yadira Tamarit <ystamaritq@gmail.com>
2020-03-18 21:11:03 -0500

    added new files to the project

---
+++ Develop/pseudocode.md
@@ -0,0 +1,13 @@
+When user clicks button
+    - prompt for password length
+        - if password less than 8 show error messgae and exit
+        - if password greater than 128 show error message and exit
+        - otherwise, prompt user for character types
+            - prompt if they want to use lowercase
+            - prompt if they want to use uppercase
+            - prompt if they want to use numeric
+            - prompt if they want to use special character
+            - if no character group selected show error message and exit
+            - otherwise, generate password
+                
+
--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,28 @@
 
-var length = prompt("How many caracters do you like to have in the new password? Enter a valid number");
-
 //function generate password
 function generatePassword() {
-  var caracters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  if (typeof length === 'integer' && length > 0) {
-    for (var i = 0; i <= length; i++) {
-
-    }
+  var pass_length = prompt("How many caracters do you like to have in the new password? Enter a valid number between 8 and 128");
+  if (pass_length < 8) {
+    confirm("Error, enter a valid number");
   }
 }
 
+
+// var caracters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+// var newpass = '';
+
+// var times = caracters.length;
+// if ((typeof pass_length === 'integer') && pass_length > 0) {
+//   for (var i = 1; i <= pass_length; i++) {
+//     newpass += caracters.charAt((Math.floor(Math.random)) * times);
+//   }
+// }
+// return newpass;
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
